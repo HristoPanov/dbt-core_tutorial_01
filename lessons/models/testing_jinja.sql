f4679c6af84c0e2def7abb6bc75d92f512efca71
HristoPanov <hristopanov@gmail.com>
2023-07-12 13:04:32 +0300

    added testing

--- lessons/models/testing_jinja.sql
+++ lessons/models/testing_jinja.sql
@@ -5,6 +5,7 @@
  -#}
 
  --comments here will appear
+{#
 {%- set my_long_variable %}
 	select 1 as my_col
 {%- endset %}
@@ -15,4 +16,24 @@ SELECT
 {%-for item in my_list %}
 	{{item}}{% if not loop.last %},{%endif%}
 {%- endfor %}
+-#}
+
+
+{# get all columns in a table, check if they start with 'total'#}
+{% set columns = adapter.get_columns_in_relation(ref('dim_orders'))%}
+
+SELECT
+{%-for column in columns %}
+	{%-if column.name.startswith('total')%}
+	{{column.name.lower()}}
+	{%- endif -%}
+{%- endfor %}
+
+{# get all values from a column in a table#}
+{% set values = dbt_utils.get_column_values(ref('dim_orders'),'order_status') %}
+{{values}}
+
+
+
+
 
